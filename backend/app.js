7782df0a32746c6d99ea3866faaa0fd3a72a1cac
unknown <damydavo2015@gmail.com>
2023-12-09 14:35:49 +0100

    update project

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,6 @@ import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 8000
 
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
 
 const app = express();
 app.use(cookieParser());

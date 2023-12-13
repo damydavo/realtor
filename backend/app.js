e4c6ac9b6d89256d0ae6a5499a09c269f534a6ab
unknown <damydavo2015@gmail.com>
2023-12-13 18:05:33 +0100

    updated project

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoute from "./routes/userRoute.js"
 import listingRoute from "./routes/listingRoute.js"
 import cookieParser from 'cookie-parser';
-import path from "path";
+// import path from "path";
 
 const port = process.env.PORT || 5000
 
@@ -17,17 +17,17 @@ app.use(express.urlencoded({ extended: true }))
 dotenv.config()
 connectDB()
 
-// Your code
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
+// // Your code
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
+//             if (err) {
+//                 res.status(500).send(err)
+//             }
+//         });
+//     })
+// }
 // Your code
 
 

6adad7292136a3a99a6cf0c231066c9037c01708
unknown <damydavo2015@gmail.com>
2023-12-13 18:24:13 +0100

    updated project

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,9 @@ connectDB()
 
 // Your code
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), function (err) {
             if (err) {
                 res.status(500).send(err)
             }

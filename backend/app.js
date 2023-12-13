65f3be1568899f08a83f654fb2dd0abdaf0276d2
unknown <damydavo2015@gmail.com>
2023-12-13 18:32:22 +0100

    updated project

--- backend/app.js
+++ backend/app.js
@@ -17,20 +17,6 @@ app.use(express.urlencoded({ extended: true }))
 dotenv.config()
 connectDB()
 
-// Your code
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), function (err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
-// Your code
-
-
 app.use('/api/users', userRoute)
 app.use('/api/listing', listingRoute)
 

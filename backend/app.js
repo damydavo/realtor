8043851ac09d284b5bbcfeb72990737ecbea06cb
unknown <damydavo2015@gmail.com>
2023-12-16 17:26:54 +0100

    updated project

--- backend/app.js
+++ backend/app.js
@@ -29,13 +29,9 @@ app.use('/api/listing', listingRoute)
 // })
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), function (err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        });
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", 'build', 'index.html'))
     })
 }
 

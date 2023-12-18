97f12f8f3b3e2223f6c348e69e34608731b13d01
unknown <damydavo2015@gmail.com>
2023-12-18 13:19:06 +0100

    added new updates

--- backend/app.js
+++ backend/app.js
@@ -17,27 +17,15 @@ app.use(express.urlencoded({ extended: true }))
 dotenv.config()
 connectDB()
 
+const __dirname = path.resolve();
 
 app.use('/api/users', userRoute)
 app.use('/api/listing', listingRoute)
 app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: __dirname + '/.env' });
-}
-
-// all your routes should go here
-app.use('/some-route', require(path.join(__dirname, 'api', 'routes', 'route.js')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "frontend", 'build', 'index.html'))
-// })
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend', 'build')));
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", 'build', 'index.html'))
+})
 
 
 app.use(notFound);

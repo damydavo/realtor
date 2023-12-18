fb1dccbdee3c7de05d3887f4d0bc1984258360cc
unknown <damydavo2015@gmail.com>
2023-12-18 17:14:56 +0100

    added new updates

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,10 @@ app.use('/api/users', userRoute)
 app.use('/api/listing', listingRoute)
 
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("*", function (_, res) {
     res.sendFile(
-        path.join(__dirname, "./frontend/build/index.html"),
+        path.join(__dirname, "frontend/build/index.html"),
         function (err) {
             res.status(500).send(err);
         }

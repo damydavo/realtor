902ff795a098a1db910775c0584c87acb02c4ab1
unknown <damydavo2015@gmail.com>
2023-12-25 13:14:21 +0100

    added project

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,24 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoute from "./routes/userRoute.js"
 import listingRoute from "./routes/listingRoute.js"
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+const port = process.env.PORT || 5000
+
+app.use(cors(
+    {
+        origin: ["https://localhost:5000"],
+        method: ["POST", "GET"],
+        credentials: true
+    }
+))
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
 
 dotenv.config()
 connectDB()
@@ -18,10 +31,10 @@ app.use('/api/users', userRoute)
 app.use('/api/listing', listingRoute)
 
 app.get('/', (req, res) => {
-    res.send("project started")
+    res.json("Hello")
 })
 
 app.use(notFound);
 app.use(errorHandler)
 
-export default app;
+app.listen(port, () => console.log(`app is listening on port ${port}`));

3ab73b7f4923bd365ccb0a45dbe3537e7c861be2
unknown <damydavo2015@gmail.com>
2023-12-31 16:57:00 +0100

    added project

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoute from "./routes/userRoute.js"
 import listingRoute from "./routes/listingRoute.js"
 import cookieParser from 'cookie-parser';
-import path from 'path'
 
 const port = process.env.PORT || 5000
 
@@ -17,22 +16,9 @@ app.use(express.urlencoded({ extended: true }))
 dotenv.config()
 connectDB()
 
-const __dirname = path.resolve()
-
 app.use('/api/users', userRoute)
 app.use('/api/listing', listingRoute)
 
-//serve the frontend
-if(process.env.NODE_ENV === 'production') {
-    //set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
-} else {
-    app.get('/', (req, res) => {
-        res.status(200).json({ message: "Welcome to realtor" })
-    })
-}
 
 app.use(notFound);
 app.use(errorHandler)

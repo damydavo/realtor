52301ff4dbcdab7a418deadf3777bd03eb6100de
unknown <damydavo2015@gmail.com>
2023-12-31 17:18:15 +0100

    added project

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,13 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoute from "./routes/userRoute.js"
 import listingRoute from "./routes/listingRoute.js"
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors'
 const port = process.env.PORT || 5000
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 dotenv.config()
